8982e27e618b4695378c691cd6da9d85d919ed76
ssin610 <ssin610@aucklanduni.ac.nz>
2021-03-18 20:01:09 +1300

    Created the file structure

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Nav} from './navbar.elements';
+
+
+const navbar = () => {
+    return (
+        
+        <Nav>placeholder</Nav>
+        
+    );
+}
+
+export default navbar
