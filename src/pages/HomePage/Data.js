718d70d4edd996b22ef1fbf679925a5fc2cbab32
ssin610 <ssin610@aucklanduni.ac.nz>
2021-04-16 20:03:17 +1200

    Added and styled the home page

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,17 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'SV Singh',
+    headline: 'Penultimate Year Software Engineering Student',
+    description:
+      'If you are interested in getting in touch, click the button below!.',
+    buttonLabel: 'Contact Me',
+    // imgStart: '',
+    // img: require('../../images/svg-1.svg'),
+    // alt: 'Credit Card',
+    // start: ''
+   
+  };
