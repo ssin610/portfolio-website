f0383e12eda3b8bfdc37ccd5bcc021ce0f24f65e
ssin610 <ssin610@aucklanduni.ac.nz>
2021-04-24 17:05:35 +1200

    Merge branch

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Navbar} from './Navbar/Navbar';
-export {default as InfoSection} from './InfoSection/InfoSection';
-export {default as Footer} from './Footer/Footer';
---
+++ src/content_option.js
@@ -0,0 +1,76 @@
+const logotext = "SV";
+const meta = {
+    title: "SV Singh",
+    description: "I’m SV Singh engineering student _ ,currently studying at The University of Auckland",
+};
+
+const introdata = {
+    title: "I’m SV",
+    animated: {
+        first: "I love coding.",
+        second: "I'm eager to learn.",
+        third: "I'm a tech enthusiast.",
+    },
+    description: "Currently a Penultimate Year Software Engineering Student at The University of Auckland",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Hello! I'm in the Penultimate Year of my Software Engineering Degree at \n The University of Auckland. I'm passionate about all things tech & design\n related and have a strong desire to learn.",
+};
+
+
+const skills = [{
+        name: "Java",
+        value: 90,
+    },
+    {
+        name: "ReactJS",
+        value: 85,
+    },
+    {
+        name: "C#",
+        value: 80,
+    },
+    {
+        name: "SQL",
+        value: 60,
+    },
+    {
+        name: "Agile/Scrum Methodology",
+        value: 85,
+    },
+];
+
+
+
+
+
+const contactConfig = {
+    YOUR_EMAIL: "svsingh.software@gmail.com",
+    YOUR_FONE: "(+64) 22 156 6137",
+    description: "Feel free to contact me by filling out the form! I'll aim to reply ASAP ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/sv-singh",
+
+    linkedin: "https://linkedin.com/in/sv-singh",
+
+};
+export {
+    meta,
+    dataabout,
+
+   
+    skills,
+  
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
